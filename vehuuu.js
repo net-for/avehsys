d39661c23072bec7f791144ccf7f4581a6fc60de
net <114216794+net-for@users.noreply.github.com>
2025-01-17 04:44:45 +0400

    Update vehuuu.js

--- vehuuu.js
+++ vehuuu.js
@@ -1,14 +1,10 @@
-// Function to close the CEF window
-function closeWindow() {
-    cef_emit_event("onCloseVevuu");
-}
+document.getElementById("closeButton").addEventListener("click", () => {
+    cef.emit("OnCloseVehuu");
+});
 
-// Function to spawn a vehicle
-function spawnVehicle() {
+document.getElementById("spawnButton").addEventListener("click", () => {
     const vehicleId = document.getElementById("vehicleId").value;
     if (vehicleId) {
-        cef_emit_event("onSpawnVehicle", parseInt(vehicleId));
-    } else {
-        alert("Please enter a valid vehicle ID.");
+        cef.emit("OnSpawnVehicle", parseInt(vehicleId));
     }
-}
+});
